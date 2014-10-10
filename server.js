197ae32ba7270e96cc0d10200b9b873cf3427631
William Fisher <williamfisher@hotmail.co.uk>
2014-10-10 12:25:33 +0100

    adding mongo db to server

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
-var serverTools = require("./servermodule.js");
-
+var serverTools = require("./servermodule2.js");
+serverTools.getFromDB(7);
+//This needs to be called ON REQUEST!
 serverTools.getTwitterData("collectiveacademy");
 
 var port = process.env.PORT || 3000;
--- servermodule.js
+++ servermodule.js
@@ -16,13 +16,18 @@ var serverTools = module.exports = {
 	},
 
 	makeServer: function makeServer (port) {
-		http.createServer(function (request, response) {
+		var server = http.createServer(function (request, response) {
 			response.writeHead(200, {
-				"Content-Type": "text/plain",
+				"Content-Type": "application/json",
+				//SEE ABOUT CHANGING THIS TO ONE SITE
 				"Access-Control-Allow-Origin": "*"
 			});
-			response.end(JSON.stringify(serverTools.tweetStore));
-		}).listen(port);
+			serverTools.getTwitterData("collectiveacademy");
+			serverTools.addToDB(serverTools.tweetStore);
+			serverTools.getFromDB(7);
+			response.write(JSON.stringify(serverTools.tweetDelivery.payload));
+		});
+		server.listen(port);
 	},
 
 	getTwitterData: function getTwitterData (search) {
@@ -34,11 +39,11 @@ var serverTools = module.exports = {
 			allTweets.statuses.forEach(function (tweet) {
 				if(tweet.entities && tweet.entities.media && tweet.entities.media[0].type === "photo"){
 					var validTweet = {
-					id: tweet.id,
-					username: tweet.user.screen_name,
-					image: tweet.entities.media[0].media_url,
-					date: tweet.created_at,
-					body: tweet.text
+						id: tweet.id,
+						username: tweet.user.screen_name,
+						image: tweet.entities.media[0].media_url,
+						date: tweet.created_at,
+						body: tweet.text
 					};
 					serverTools.tweetStore.newTweet(validTweet);
 				}
@@ -48,43 +53,51 @@ var serverTools = module.exports = {
 	}, 
 
 	addToDB: function addToDB (addDocument) {
-		var mongoURI = "mongodb://<chaps>:<ontwitter>@linus.mongohq.com:10089/chaps-twitter";
+		var mongoURI = require("./mongo.js").url;
 		mongo.MongoClient.connect(mongoURI, function (err, db) {
 				var collection = db.collection("tweets");
 				Object.keys(addDocument).forEach(function (tweet) {
-					var tweetExists = collection.find( { "id": addDocument[tweet] } );
-					if (tweetExists) {
-						return;
-					} else {
-						collection.insert(addDocument[tweet]);
-					}
-				});	
+					collection.find( { id: addDocument[tweet].id }, function (err, cursor) {
+						cursor.toArray(function(err, items){
+							if(!items.length){
+								collection.insert(addDocument[tweet], function (err, result) {});
+							}
+						})
+					});
+				});
 		});
 		
 	},
-
+/*
 	removeFromDB: function removeFromDB (tweetID) {
-		var mongoURI = "mongodb://<chaps>:<ontwitter>@linus.mongohq.com:10089/chaps-twitter";
+		var mongoURI = require("./mongo.js").url;
 		mongo.MongoClient.connect(mongoURI, function (err, db) {
 			var collection = db.collection("tweets");
 			collection.remove( { "id": tweetID } );
 		});
 	},
-
+*/
 	getFromDB: function getFromDB (quantity) {
-		var mongoURI = "mongodb://<chaps>:<ontwitter>@linus.mongohq.com:10089/chaps-twitter";
+		var mongoURI = require("./mongo.js").url;
 		mongo.MongoClient.connect(mongoURI, function (err, db) {
 			var collection = db.collection("tweets");
-			return collection.find().sort( { "date": -1 } ).limit(quantity);
-		});
+			collection.find().sort( { "date": -1 } ).limit(quantity).toArray(function (err, items) {
+				//console.log(items);
+				serverTools.tweetDelivery.payload = items;
+			});
+		})
 	},
 
+	tweetDelivery: {
+
+	}
+/*
 	connectToMongo: function connectToMongo (callback) {
-		var mongoURI = "mongodb://<chaps>:<ontwitter>@linus.mongohq.com:10089/chaps-twitter";
+		var mongoURI = require("./mongo.js").url;
 		mongo.MongoClient.connect(mongoURI, function (err, db) {
 			callback();
 		});
 	}
-
+*/
 };
 
