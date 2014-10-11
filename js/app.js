089a6bb5b75e6802f0eda98582ea5b92d3256732
William Fisher <williamsykesfisher@gmail.com>
2014-10-11 20:31:19 +0100

    Update app.js
    testing timeago

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 function main () {
 
-	getContent("http://chaps-server.herokuapp.com/");
+	getContent("http://chaps-server.herokuapp.com/");	
 
 }
 
@@ -15,8 +15,35 @@ function getContent (url) {
 
 }
 
+function timeSince(date) {
+
+    var seconds = Math.floor((new Date() - date) / 1000);
+
+    var interval = Math.floor(seconds / 31536000);
+
+    if (interval > 1) {
+        return interval + " years ago";
+    }
+    interval = Math.floor(seconds / 2592000);
+    if (interval > 1) {
+        return interval + " months ago";
+    }
+    interval = Math.floor(seconds / 86400);
+    if (interval > 1) {
+        return interval + " days ago";
+    }
+    interval = Math.floor(seconds / 3600);
+    if (interval > 1) {
+        return interval + " hours ago";
+    }
+    interval = Math.floor(seconds / 60);
+    if (interval > 1) {
+        return interval + " minutes ago";
+    }
+    return Math.floor(seconds) + " seconds ago";
+}
+
 function putInObjects (data) {
-	//sortdata and create big html
 	var counter = 0;
 	var objectified = {};
 	var property;
@@ -31,7 +58,7 @@ function putInObjects (data) {
 function constructContent (object) {
 	var content = "";
 	Object.keys(object).forEach(function (key) {
-		
+	var elapsed = 
 		switch (key) {
 			case "tweet1":
 				content += '<div class="row firstrow">' +
@@ -40,41 +67,41 @@ function constructContent (object) {
 					object[key].username + '</a></p><a href="' + object[key].image +
 					'" class="thetweet" target="_blank"><img src="' + object[key].image + 
 					'"></a></div><div class="col-md-4 text-wrapper"><p class="tweet-text">' + 
-					object[key].body + '</p><p class="date">' + object[key].date + '</p></div></div>';
+					object[key].body + '</p><p class="date">' + timeSince(object[key].date) + '</p></div></div>';
 				break;
 			case "tweet2":
 				content += '<div class="col-xs-10 col-xs-offset-1 col-md-5 col-md-offset-1 post" id="post2">' +
-                			'<div class="col-xs-12 img-frame"><p class="username"><a href="#" class="user">' + 
-         				object[key].username + '</a></p><a href="' + object[key].image + '" class="thetweet"' +
-                			' target="_blank"><img src="' + object[key].image + '"></a></div><div class="col-xs-12' + 
-                			' text-wrapper"><p class="tweet-text">' + object[key].body + '</p><p class="date">' + 
-                			object[key].date + '</p></div></div></div>';
-                		break;
-            		case "tweet3":
-            			content += '<div class="row secondrow"><div class="col-xs-10 col-xs-offset-1 col-md-6' + 
-            				' col-md-offset-0 post" id="post3"><div class="col-md-8 col-md-push-4 img-frame">' + 
-            				'<p class="username"><a href="#" class="user">' + object[key].username + '</a></p>' + 
-            				'<a href="' + object[key].image + '" class="thetweet" target="_blank"><img src="' + 
-            				object[key].image + '"></a></div><div class="col-md-4 col-md-pull-8' +
-            				' text-wrapper"><p class="tweet-text">' + object[key].body + '</p><p class="date">' + 
-            				object[key].date + '</p></div></div></div>';
-            			break;
-            		case "tweet4":
-            			content += '<div class="row thirdrow"><div class="col-md-6"><div class="col-xs-10' + 
-            				' col-xs-offset-1 col-md-11 col-md-offset-0 post" id="post4"><div class="col-xs-12' + 
-            				' img-frame"><p class="username"><a href="#" class="user">' + object[key].username + 
-            				'</a></p><a href="' + object[key].image + '" class="thetweet" target="_blank">' +
-            				'<img src="' + object[key].image + '" class=""></a></div><div class="col-xs-12' +
-            				' text-wrapper"><p class="tweet-text">' + object[key].body + '</p><p class="date">' +
-            				'</p></div></div></div>';
-            			break;
-            		case "tweet5":
-            			content += '<div class="col-md-6"><div class="col-xs-10 col-xs-offset-1 col-md-11' +
-            				' col-md-offset-1 post" id="post5"><div class="col-xs-12 img-frame"><p class="username">' +
-        			    	'<a href="#" class="user">' + object[key].username + '</a></p><a href="' + object[key].image +
-        			    	'" class="thetweet" target="_blank"><img src="' + object[key].image +'" class=""></a>' +
-        			    	'</div><div class="col-xs-12 text-wrapper"><p class="tweet-text">' + object[key].body + 
-        			    	'</p><p class="date">' + object[key].date + '</p></div></div></div></div>'
+                	'<div class="col-xs-12 img-frame"><p class="username"><a href="#" class="user">' + 
+                	object[key].username + '</a></p><a href="' + object[key].image + '" class="thetweet"' +
+                	' target="_blank"><img src="' + object[key].image + '"></a></div><div class="col-xs-12' + 
+                	' text-wrapper"><p class="tweet-text">' + object[key].body + '</p><p class="date">' + 
+                	timeSince(object[key].date) + '</p></div></div></div>';
+                break;
+            case "tweet3":
+            	content += '<div class="row secondrow"><div class="col-xs-10 col-xs-offset-1 col-md-6' + 
+            		' col-md-offset-0 post" id="post3"><div class="col-md-8 col-md-push-4 img-frame">' + 
+            		'<p class="username"><a href="#" class="user">' + object[key].username + '</a></p>' + 
+            		'<a href="' + object[key].image + '" class="thetweet" target="_blank"><img src="' + 
+            		object[key].image + '"></a></div><div class="col-md-4 col-md-pull-8' +
+            		' text-wrapper"><p class="tweet-text">' + object[key].body + '</p><p class="date">' + 
+            		timeSince(object[key].date) + '</p></div></div></div>';
+            	break;
+            case "tweet4":
+            	content += '<div class="row thirdrow"><div class="col-md-6"><div class="col-xs-10' + 
+            		' col-xs-offset-1 col-md-11 col-md-offset-0 post" id="post4"><div class="col-xs-12' + 
+            		' img-frame"><p class="username"><a href="#" class="user">' + object[key].username + 
+            		'</a></p><a href="' + object[key].image + '" class="thetweet" target="_blank">' +
+            		'<img src="' + object[key].image + '" class=""></a></div><div class="col-xs-12' +
+            		' text-wrapper"><p class="tweet-text">' + object[key].body + '</p><p class="date">' +
+            		timeSince(object[key].date) + '</p></div></div></div>';
+            	break;
+            case "tweet5":
+            	content += '<div class="col-md-6"><div class="col-xs-10 col-xs-offset-1 col-md-11' +
+            		' col-md-offset-1 post" id="post5"><div class="col-xs-12 img-frame"><p class="username">' +
+            		'<a href="#" class="user">' + object[key].username + '</a></p><a href="' + object[key].image +
+            		'" class="thetweet" target="_blank"><img src="' + object[key].image +'" class=""></a>' +
+            		'</div><div class="col-xs-12 text-wrapper"><p class="tweet-text">' + object[key].body + 
+            		'</p><p class="date">' + timeSince(object[key].date) + '</p></div></div></div></div>'
 		}
 	
 	})
