ffd18973dc5a54bc1a063888e331e4804f41b729
William Fisher <williamsykesfisher@gmail.com>
2014-10-11 20:36:39 +0100

    Update app.js
    removed unfunctioning timeago

--- js/app.js
+++ js/app.js
@@ -15,46 +15,6 @@ function getContent (url) {
 
 }
 
-function timeSince(date) {
-
-    var seconds = Math.floor((new Date() - date) / 1000);
-
-    var interval = Math.floor(seconds / 31536000);
-
-    if (interval > 1) {
-        return interval + " years ago";
-    }
-    interval = Math.floor(seconds / 2592000);
-    if (interval > 1) {
-        return interval + " months ago";
-    }
-    interval = Math.floor(seconds / 86400);
-    if (interval > 1) {
-        return interval + " days ago";
-    }
-    interval = Math.floor(seconds / 3600);
-    if (interval > 1) {
-        return interval + " hours ago";
-    }
-    interval = Math.floor(seconds / 60);
-    if (interval > 1) {
-        return interval + " minutes ago";
-    }
-    return Math.floor(seconds) + " seconds ago";
-}
-
-function putInObjects (data) {
-	var counter = 0;
-	var objectified = {};
-	var property;
-	data.forEach(function (tweet) {
-		counter += 1;
-		property = "tweet" + counter;
-		objectified[property] = tweet;
-	});
-	constructContent(objectified);
-}
-
 function constructContent (object) {
 	var content = "";
 	Object.keys(object).forEach(function (key) {
@@ -67,7 +27,7 @@ function constructContent (object) {
 					object[key].username + '</a></p><a href="' + object[key].image +
 					'" class="thetweet" target="_blank"><img src="' + object[key].image + 
 					'"></a></div><div class="col-md-4 text-wrapper"><p class="tweet-text">' + 
-					object[key].body + '</p><p class="date">' + timeSince(object[key].date) + '</p></div></div>';
+					object[key].body + '</p><p class="date">' + object[key].date + '</p></div></div>';
 				break;
 			case "tweet2":
 				content += '<div class="col-xs-10 col-xs-offset-1 col-md-5 col-md-offset-1 post" id="post2">' +
@@ -75,7 +35,7 @@ function constructContent (object) {
                 	object[key].username + '</a></p><a href="' + object[key].image + '" class="thetweet"' +
                 	' target="_blank"><img src="' + object[key].image + '"></a></div><div class="col-xs-12' + 
                 	' text-wrapper"><p class="tweet-text">' + object[key].body + '</p><p class="date">' + 
-                	timeSince(object[key].date) + '</p></div></div></div>';
+                	object[key].date + '</p></div></div></div>';
                 break;
             case "tweet3":
             	content += '<div class="row secondrow"><div class="col-xs-10 col-xs-offset-1 col-md-6' + 
@@ -84,7 +44,7 @@ function constructContent (object) {
             		'<a href="' + object[key].image + '" class="thetweet" target="_blank"><img src="' + 
             		object[key].image + '"></a></div><div class="col-md-4 col-md-pull-8' +
             		' text-wrapper"><p class="tweet-text">' + object[key].body + '</p><p class="date">' + 
-            		timeSince(object[key].date) + '</p></div></div></div>';
+            		object[key].date + '</p></div></div></div>';
             	break;
             case "tweet4":
             	content += '<div class="row thirdrow"><div class="col-md-6"><div class="col-xs-10' + 
@@ -93,7 +53,7 @@ function constructContent (object) {
             		'</a></p><a href="' + object[key].image + '" class="thetweet" target="_blank">' +
             		'<img src="' + object[key].image + '" class=""></a></div><div class="col-xs-12' +
             		' text-wrapper"><p class="tweet-text">' + object[key].body + '</p><p class="date">' +
-            		timeSince(object[key].date) + '</p></div></div></div>';
+            		object[key].date + '</p></div></div></div>';
             	break;
             case "tweet5":
             	content += '<div class="col-md-6"><div class="col-xs-10 col-xs-offset-1 col-md-11' +
@@ -101,7 +61,7 @@ function constructContent (object) {
             		'<a href="#" class="user">' + object[key].username + '</a></p><a href="' + object[key].image +
             		'" class="thetweet" target="_blank"><img src="' + object[key].image +'" class=""></a>' +
             		'</div><div class="col-xs-12 text-wrapper"><p class="tweet-text">' + object[key].body + 
-            		'</p><p class="date">' + timeSince(object[key].date) + '</p></div></div></div></div>'
+            		'</p><p class="date">' + object[key].date + '</p></div></div></div></div>'
 		}
 	
 	})
