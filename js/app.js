0111c2086cd72f18ab0f6411644ead9971dd5ac1
William Fisher <williamfisher@hotmail.co.uk>
2014-10-11 19:29:54 +0100

    removing console.logs

--- js/app.js
+++ js/app.js
@@ -1,7 +1,6 @@
 function main () {
 
-	getContent("http://chaps-server.herokuapp.com/");
-	//$("#test").html("zdereuo")	
+	getContent("http://chaps-server.herokuapp.com/");	
 
 }
 
@@ -10,7 +9,6 @@ function getContent (url) {
 	$.ajax({
 		url: url,
 		success: function (data) {
-			console.log(data);
 			putInObjects(JSON.parse(data));
 		}
 	});
@@ -36,7 +34,6 @@ function constructContent (object) {
 		
 		switch (key) {
 			case "tweet1":
-				console.log("first");
 				content += '<div class="row firstrow">' +
 					'<div class="col-xs-10 col-xs-offset-1 col-md-6 col-md-offset-0 post" id="post1">' +
 					'<div class="col-md-8 img-frame"><p class="username"><a href="#" class="user">' +
@@ -46,7 +43,6 @@ function constructContent (object) {
 					object[key].body + '</p><p class="date">' + object[key].date + '</p></div></div>';
 				break;
 			case "tweet2":
-				console.log("second");
 				content += '<div class="col-xs-10 col-xs-offset-1 col-md-5 col-md-offset-1 post" id="post2">' +
                 	'<div class="col-xs-12 img-frame"><p class="username"><a href="#" class="user">' + 
                 	object[key].username + '</a></p><a href="' + object[key].image + '" class="thetweet"' +
@@ -55,7 +51,6 @@ function constructContent (object) {
                 	object[key].date + '</p></div></div></div>';
                 break;
             case "tweet3":
-            	console.log("third");
             	content += '<div class="row secondrow"><div class="col-xs-10 col-xs-offset-1 col-md-6' + 
             		' col-md-offset-0 post" id="post3"><div class="col-md-8 col-md-push-4 img-frame">' + 
             		'<p class="username"><a href="#" class="user">' + object[key].username + '</a></p>' + 
@@ -65,7 +60,6 @@ function constructContent (object) {
             		object[key].date + '</p></div></div></div>';
             	break;
             case "tweet4":
-            	console.log("fourth");
             	content += '<div class="row thirdrow"><div class="col-md-6"><div class="col-xs-10' + 
             		' col-xs-offset-1 col-md-11 col-md-offset-0 post" id="post4"><div class="col-xs-12' + 
             		' img-frame"><p class="username"><a href="#" class="user">' + object[key].username + 
@@ -75,7 +69,6 @@ function constructContent (object) {
             		'</p></div></div></div>';
             	break;
             case "tweet5":
-            	console.log("fifth");
             	content += '<div class="col-md-6"><div class="col-xs-10 col-xs-offset-1 col-md-11' +
             	' col-md-offset-1 post" id="post5"><div class="col-xs-12 img-frame"><p class="username">' +
             	'<a href="#" class="user">' + object[key].username + '</a></p><a href="' + object[key].image +
@@ -86,7 +79,6 @@ function constructContent (object) {
 	
 	})
 
-	//$("#test").html(content);
 	injectContent(content);
 }
 
